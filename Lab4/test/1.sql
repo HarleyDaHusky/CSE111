edcb9bc0b63643b6c46a815d4129776a4b76ab4e
Jeffrey Peng <121198129+HarleyDaHusky@users.noreply.github.com>
2025-01-14 15:49:57 -0800

    Delete Lab4 directory

--- Lab4/test/1.sql
+++
@@ -1,9 +0,0 @@
-.headers on
---How many orders are posted by customers in every country in MIDDLE EAST?
-SELECT DISTINCT n.n_name as country, count(n.n_name) as cnt
-FROM region r
-JOIN nation n on n.n_regionkey = r.r_regionkey
-JOIN customer c on c.c_nationkey = n.n_nationkey
-JOIN orders o on o.o_custkey = c.c_custkey
-WHERE r.r_name = 'MIDDLE EAST'
-GROUP BY n.n_name;
--- Lab4/test/5.sql
+++
@@ -1,7 +0,0 @@
-.headers on
---Find the number of suppliers from each of JAPAN and CHINA
-SELECT n.n_name as country, count(n.n_name) as cnt
-FROM nation n 
-JOIN supplier s on s.s_nationkey = n.n_nationkey
-WHERE n.n_name = 'JAPAN' or n.n_name = 'CHINA'
-GROUP BY n.n_name;
